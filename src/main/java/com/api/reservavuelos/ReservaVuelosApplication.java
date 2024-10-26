b2e2bfbb9799629613de7995181d51711113f6dc
wubbzy25 <carlosasalas321@gmail.com>
2024-10-25 23:39:05 -0500

    add dev containers

--- src/main/java/com/api/reservavuelos/ReservaVuelosApplication.java
+++ src/main/java/com/api/reservavuelos/ReservaVuelosApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cache.annotation.EnableCaching;
 @SpringBootApplication
 @EnableCaching
 public class ReservaVuelosApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ReservaVuelosApplication.class, args);
     }
-
 }
